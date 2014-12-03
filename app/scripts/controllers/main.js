62341ad3ee589837c262fdcfa237be08a92bb252
Kristjan <liiva.kristjan+github@gmail.com>
2014-12-03 10:32:51 +0100

    Updated the requirements list and added profile panel and notifications panel. Reworked the controller data structures to match the DB model.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,31 +9,30 @@
  */
 angular.module('requirementsBazaarWebFrontendApp')
     .controller('MainCtrl', function ($scope) {
+
+    //Currently logged in user
+    $scope.activeUser = {id : '000', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'};
+
+    //activeproject holds the project that is currently selected and projects everything else
+    $scope.activeProject = {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '000'};
     $scope.projects = [
-      {name : 'Project 1', description : 'This is a cool project 1'},
-      {name : 'Project 2', description : 'This is a cool project 2'},
-      {name : 'Project 3', description : 'This is a cool project 3'},
-      {name : 'Project 4', description : 'This is a cool project 4'}
-    ];
-    $scope.activeProjectComponents = [
-      {name : 'Component 1', description : 'This is a cool component 1'},
-      {name : 'Component 2', description : 'This is a cool component 2'},
-      {name : 'Component 3', description : 'This is a cool component 3'},
-      {name : 'Component 4', description : 'This is a cool component 4'},
-      {name : 'Component 5', description : 'This is a cool component 5'},
-      {name : 'Component 6', description : 'This is a cool component 6'},
-      {name : 'Component 7', description : 'This is a cool component 7'},
-      {name : 'Component 8', description : 'This is a cool component 8'}
+      {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '000'},
+      {id : '2',name : 'Project 2', description : 'This is a cool project 2', visibility : '+', leader : '000'}
     ];
 
+    //activeComponent holds the component that is currently selected and components everythings else from the active project
     $scope.activeComponent = {name : 'Component 1', description : 'This is a cool component 1'};
+    $scope.components = [
+      {name : 'Component 1', description : 'This is a cool component 1'},
+      {name : 'Component 2', description : 'This is a cool component 2'}
+    ];
 
-    $scope.activeProject = {name : 'Project 1', description : 'This is a cool project 1'};
+
 
     $scope.activeComponentRequirements = [
-      {name : 'Requirement 1', text : 'Please update the ui'},
-      {name : 'Requirement 2', text : 'It does not work'},
-      {name : 'Requirement 3', text : 'Still not working'}
+      {id : '000', name : 'Requirement 1', description : 'Please update the ui 1', Project_Id : '000', Leader_Id : '000'},
+      {id : '001', name : 'Requirement 2', description : 'Please update the ui 2', Project_Id : '000', Leader_Id : '000'},
+      {id : '002', name : 'Requirement 3', description : 'Please update the ui 3', Project_Id : '000', Leader_Id : '000'}
     ];
 
     $scope.selectComponent = function(name, description){
@@ -43,30 +42,9 @@ angular.module('requirementsBazaarWebFrontendApp')
       //TODO Rewrite the activeComponentRequirements
       //Either request them from server or take ones saved locally
       $scope.activeComponentRequirements = [
-        {name : 'Requirement 1 alternative', text : 'Please update the ui'},
-        {name : 'Requirement 2 alternative', text : 'It does not work'},
-        {name : 'Requirement 3 alternative', text : 'Still not working'},
-        {name : 'Requirement 4 alternative', text : 'Please update the ui 2'},
-        {name : 'Requirement 5 alternative', text : 'It does not work 2 '},
-        {name : 'Requirement 6 alternative', text : 'Still not working 2'},
-        {name : 'Requirement 1 alternative', text : 'Please update the ui'},
-        {name : 'Requirement 2 alternative', text : 'It does not work'},
-        {name : 'Requirement 3 alternative', text : 'Still not working'},
-        {name : 'Requirement 4 alternative', text : 'Please update the ui 2'},
-        {name : 'Requirement 5 alternative', text : 'It does not work 2 '},
-        {name : 'Requirement 6 alternative', text : 'Still not working 2'},
-        {name : 'Requirement 1 alternative', text : 'Please update the ui'},
-        {name : 'Requirement 2 alternative', text : 'It does not work'},
-        {name : 'Requirement 3 alternative', text : 'Still not working'},
-        {name : 'Requirement 4 alternative', text : 'Please update the ui 2'},
-        {name : 'Requirement 5 alternative', text : 'It does not work 2 '},
-        {name : 'Requirement 6 alternative', text : 'Still not working 2'},
-        {name : 'Requirement 1 alternative', text : 'Please update the ui'},
-        {name : 'Requirement 2 alternative', text : 'It does not work'},
-        {name : 'Requirement 3 alternative', text : 'Still not working'},
-        {name : 'Requirement 4 alternative', text : 'Please update the ui 2'},
-        {name : 'Requirement 5 alternative', text : 'It does not work 2 '},
-        {name : 'Requirement 6 alternative', text : 'Still not working 2'}
+        {id : '003', name : 'Requirement 4', description : 'UI is wack 1', Project_Id : '000', Leader_Id : '000'},
+        {id : '004', name : 'Requirement 5', description : 'UI is wack 2', Project_Id : '000', Leader_Id : '000'},
+        {id : '005', name : 'Requirement 6', description : 'UI is wack 3', Project_Id : '000', Leader_Id : '000'}
       ];
 
     };
@@ -75,13 +53,21 @@ angular.module('requirementsBazaarWebFrontendApp')
       $scope.activeProject.name = name;
       $scope.activeProject.description = description;
 
-      //TODO Rewrite activeProjectComponents
+      //TODO Rewrite components
       //Either request them from server or take ones saved locally
-      $scope.activeProjectComponents = [
+      $scope.components = [
         {name : 'Component 1', description : 'This is a cool component 1'},
         {name : 'Component 2', description : 'This is a cool component 2'},
         {name : 'Component 3', description : 'This is a cool component 3'}
       ];
+    };
+
+    $scope.toggle = function(clickEvent) {
+      clickEvent.toElement.nextElementSibling .toggle();
+    };
+
+    $scope.signOut = function(){
+      window.alert('TODO sign out');
     };
 
   });
