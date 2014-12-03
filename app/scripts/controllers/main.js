6a74dfdb5787d0a96b7633807e06b5d8a75fba3d
Kristjan <liiva.kristjan+github@gmail.com>
2014-12-03 12:02:26 +0100

    Hard coded some demo data into the js model. Fixed the project selection drop down.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,60 +10,94 @@
 angular.module('requirementsBazaarWebFrontendApp')
     .controller('MainCtrl', function ($scope) {
 
-    //Currently logged in user
-    $scope.activeUser = {id : '000', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'};
+    //activeUser is logged in user and users everyone else
+    $scope.activeUser = {id : '1', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'};
+    $scope.users = [
+      {id : '1', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'},
+      {id : '2', firstname : 'Istvan', lastname : 'Koren', email : 'koren@web.com', admin : 'true', Las2PeerId :'000', username : 'IstvanKoren'},
+      {id : '3', firstname : 'Adam', lastname : 'Gavronek', email : 'gavronek@web.com', admin : 'true', Las2PeerId :'000', username : 'AdamGavronek'}
+    ];
 
     //activeproject holds the project that is currently selected and projects everything else
-    $scope.activeProject = {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '000'};
+    $scope.activeProject = {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '1'};
     $scope.projects = [
-      {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '000'},
-      {id : '2',name : 'Project 2', description : 'This is a cool project 2', visibility : '+', leader : '000'}
+      {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '1'},
+      {id : '2',name : 'Project 2', description : 'This is a cool project 2', visibility : '+', leader : '2'}
     ];
 
     //activeComponent holds the component that is currently selected and components everythings else from the active project
-    $scope.activeComponent = {name : 'Component 1', description : 'This is a cool component 1'};
+    $scope.activeComponent = {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'};
     $scope.components = [
-      {name : 'Component 1', description : 'This is a cool component 1'},
-      {name : 'Component 2', description : 'This is a cool component 2'}
+      {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'},
+      {id : '2', name : 'Component 2', description : 'This is a cool component 2', projectId : '1', leaderId : '2'}
     ];
-
 
 
     $scope.activeComponentRequirements = [
-      {id : '000', name : 'Requirement 1', description : 'Please update the ui 1', Project_Id : '000', Leader_Id : '000'},
-      {id : '001', name : 'Requirement 2', description : 'Please update the ui 2', Project_Id : '000', Leader_Id : '000'},
-      {id : '002', name : 'Requirement 3', description : 'Please update the ui 3', Project_Id : '000', Leader_Id : '000'}
+      {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', leaderId : '1'},
+      {id : '2', name : 'Requirement 2', description : 'Please update the ui 2', projectId : '1', leaderId : '1'},
+      {id : '3', name : 'Requirement 3', description : 'Please update the ui 3', projectId : '1', leaderId : '1'}
     ];
 
-    $scope.selectComponent = function(name, description){
-      $scope.activeComponent.name = name;
-      $scope.activeComponent.description = description;
+    $scope.selectProject = function(project){
+      $scope.activeProject = project;
 
-      //TODO Rewrite the activeComponentRequirements
-      //Either request them from server or take ones saved locally
-      $scope.activeComponentRequirements = [
-        {id : '003', name : 'Requirement 4', description : 'UI is wack 1', Project_Id : '000', Leader_Id : '000'},
-        {id : '004', name : 'Requirement 5', description : 'UI is wack 2', Project_Id : '000', Leader_Id : '000'},
-        {id : '005', name : 'Requirement 6', description : 'UI is wack 3', Project_Id : '000', Leader_Id : '000'}
-      ];
+      //TODO Rewrite components
+      //Request Components with projectId
+      //Demo Data
+      if($scope.activeProject.id === '1'){
+        $scope.components = [
+          {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'},
+          {id : '2', name : 'Component 2', description : 'This is a cool component 2', projectId : '1', leaderId : '2'}
+        ];
+      }else{
+        $scope.components = [
+          {id : '3', name : 'Component 3', description : 'This is a cool component 3', projectId : '2', leaderId : '3'}
+        ];
+      }
+
+      //TODO check if empty and how to handle it?
+      $scope.activeComponent = $scope.components[0];
+
+      showRequirements();
+
+      //TODO query tags
 
     };
 
-    $scope.selectProject = function(name, description){
-      $scope.activeProject.name = name;
-      $scope.activeProject.description = description;
+    $scope.selectComponent = function(component){
+      $scope.activeComponent = component;
 
-      //TODO Rewrite components
-      //Either request them from server or take ones saved locally
-      $scope.components = [
-        {name : 'Component 1', description : 'This is a cool component 1'},
-        {name : 'Component 2', description : 'This is a cool component 2'},
-        {name : 'Component 3', description : 'This is a cool component 3'}
-      ];
+      showRequirements();
+
+      //TODO query tags
+    };
+
+    var showRequirements = function(){
+      //TODO query the requirements for the component
+      //Demo Data
+      if($scope.activeComponent.id === '1'){
+        $scope.activeComponentRequirements = [
+          {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', leaderId : '1'},
+          {id : '2', name : 'Requirement 2', description : 'Please update the ui 2', projectId : '1', leaderId : '1'},
+          {id : '3', name : 'Requirement 3', description : 'Please update the ui 3', projectId : '1', leaderId : '1'}
+        ];
+      }else if($scope.activeComponent.id === '2'){
+        $scope.activeComponentRequirements = [
+          {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', leaderId : '1'},
+          {id : '4', name : 'Requirement 4', description : 'Please update the ui 4', projectId : '1', leaderId : '1'},
+        ];
+      }else{
+        //Component 3 belong to project 2
+        $scope.activeComponentRequirements = [
+          {id : '5', name : 'Requirement 5', description : 'Please update the ui 5', projectId : '2', leaderId : '1'},
+          {id : '6', name : 'Requirement 6', description : 'Please update the ui 6', projectId : '2', leaderId : '1'}
+        ];
+      }
     };
 
     $scope.toggle = function(clickEvent) {
-      clickEvent.toElement.nextElementSibling .toggle();
+      clickEvent.toElement.nextElementSibling.toggle();
     };
 
     $scope.signOut = function(){
