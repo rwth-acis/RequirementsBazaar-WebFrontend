cbf4aab8f67888dbb693d738767410cc587d97d3
Kristjan <liiva.kristjan+github@gmail.com>
2014-12-12 11:24:37 +0100

    New layout element and added fields and buttons to add new components and requirements.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,96 +8,170 @@
  * Controller of the requirementsBazaarWebFrontendApp
  */
 angular.module('requirementsBazaarWebFrontendApp')
-    .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope, reqBazService) {
 
-    //activeUser is logged in user and users everyone else
+    $scope.projects = null;
+    $scope.components = null;
+    $scope.requirements = null;
     $scope.activeUser = {id : '1', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'};
-    $scope.users = [
-      {id : '1', firstname : 'Kristjan', lastname : 'Liiva', email : 'liiva@web.com', admin : 'true', Las2PeerId :'000', username : 'KristjanLiiva'},
-      {id : '2', firstname : 'Istvan', lastname : 'Koren', email : 'koren@web.com', admin : 'true', Las2PeerId :'000', username : 'IstvanKoren'},
-      {id : '3', firstname : 'Adam', lastname : 'Gavronek', email : 'gavronek@web.com', admin : 'true', Las2PeerId :'000', username : 'AdamGavronek'}
-    ];
 
-    //activeproject holds the project that is currently selected and projects everything else
-    $scope.activeProject = {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '1'};
-    $scope.projects = [
-      {id : '1', name : 'Project 1', description : 'This is a cool project 1', visibility : '+', leader : '1'},
-      {id : '2',name : 'Project 2', description : 'This is a cool project 2', visibility : '+', leader : '2'}
-    ];
+    $scope.projectLeader = null;
+    $scope.componentLeader = null;
 
-    //activeComponent holds the component that is currently selected and components everything else from the active project
-    $scope.activeComponent = {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'};
-    $scope.components = [
-      {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'},
-      {id : '2', name : 'Component 2', description : 'This is a cool component 2', projectId : '1', leaderId : '2'}
-    ];
+    $scope.activeProject = null;
+    $scope.activeComponent = null;
 
-    $scope.requirements = [
-      {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', creatorId : '1'},
-      {id : '2', name : 'Requirement 2', description : 'Please update the ui 2', projectId : '1', creatorId : '2'},
-      {id : '3', name : 'Requirement 3', description : 'Please update the ui 3', projectId : '1', creatorId : '3'}
-    ];
+    //init functions that need to be run when the user enters the page
+    function getProjects(){
+      reqBazService.getProjects()
+        .success(function (projs) {
+          $scope.projects = projs;
+          $scope.activeProject = $scope.projects[0];
+          $scope.selectProj($scope.activeProject);
 
-    $scope.selectProject = function(project){
+        })
+        .error(function (error) {
+          $scope.status = 'Unable to load customer data: ' + error.message;
+        });
+    }
+
+    //Is called when the user selects a new project
+    $scope.selectProj = function (project) {
       $scope.activeProject = project;
 
-      //TODO Rewrite components
-      //Request Components with projectId
-      //Demo Data
-      if($scope.activeProject.id === '1'){
-        $scope.components = [
-          {id : '1', name : 'Component 1', description : 'This is a cool component 1', projectId : '1', leaderId : '1'},
-          {id : '2', name : 'Component 2', description : 'This is a cool component 2', projectId : '1', leaderId : '2'}
-        ];
-      }else{
-        $scope.components = [
-          {id : '3', name : 'Component 3', description : 'This is a cool component 3', projectId : '2', leaderId : '3'}
-        ];
-      }
-
-      //TODO check if empty and how to handle it?
-      $scope.activeComponent = $scope.components[0];
-
-      showRequirements();
-
-      //TODO query tags
-
+      reqBazService.getComponents($scope.activeProject.id)
+        .success(function (comps) {
+          $scope.components = comps;
+          $scope.activeComponent = $scope.components[0];
+          console.log($scope.activeComponent);
+          $scope.selectComp($scope.activeComponent);
+        })
+        .error(function (error) {
+          $scope.status = 'ERROR: ' + error.message;
+        });
     };
 
-    $scope.selectComponent = function(component){
+    //Is called when the user has selected a different component
+    $scope.selectComp = function (component) {
       $scope.activeComponent = component;
+      getUser($scope.activeComponent.leaderId,'component');
 
-      showRequirements();
-
-      //TODO query tags
+      reqBazService.getRequirementsByComponent($scope.activeComponent.id,$scope.activeComponent.id)
+        .success(function (reqs) {
+          $scope.requirements = reqs;
+          console.log($scope.requirements);
+        })
+        .error(function (error) {
+          $scope.status = 'ERROR: ' + error.message;
+        });
     };
 
-    var showRequirements = function(){
-      //TODO query the requirements for the component
-      //Demo Data
-      if($scope.activeComponent.id === '1'){
-        $scope.requirements = [
-          {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', creatorId : '1'},
-          {id : '2', name : 'Requirement 2', description : 'Please update the ui 2', projectId : '1', creatorId : '2'},
-          {id : '3', name : 'Requirement 3', description : 'Please update the ui 3', projectId : '1', creatorId : '3'}
-        ];
-      }else if($scope.activeComponent.id === '2'){
-        $scope.requirements = [
-          {id : '1', name : 'Requirement 1', description : 'Please update the ui 1', projectId : '1', creatorId : '1'},
-          {id : '4', name : 'Requirement 4', description : 'Please update the ui 4', projectId : '1', creatorId : '1'}
-        ];
-      }else{
-        //Component 3 belong to project 2
-        $scope.requirements = [
-          {id : '5', name : 'Requirement 5', description : 'Please update the ui 5', projectId : '2', creatorId : '3'},
-          {id : '6', name : 'Requirement 6', description : 'Please update the ui 6', projectId : '2', creatorId : '3'}
-        ];
-      }
-    };
+    //Queries for users
+    function getUser(id,purpose){
+      reqBazService.getUser(id)
+        .success(function (user) {
+          if(purpose === 'component'){
+            $scope.componentLeader = user;
+          }else if(purpose === 'project'){
+            $scope.projectLeader = user;
+          }else{
+
+          }
+        })
+        .error(function (error) {
+          console.log(error.message);
+          $scope.status = 'ERROR: ' + error.message;
+        });
+    }
+
+    //Call the init functions
+    getProjects();
 
     $scope.toggle = function(clickEvent) {
-      clickEvent.toElement.nextElementSibling.toggle();
+      var collapse = clickEvent.toElement.nextElementSibling;
+
+      if(collapse.getAttribute('data-visible') === 'false'){
+        console.log('opened requirement');
+        collapse.setAttribute('data-visible', 'true');
+
+        //Get all the missing pieces of information, like leader, follower, votes
+
+      }else{
+        collapse.setAttribute('data-visible', 'false');
+      }
+
+      //toggle visibility of the requirement
+      collapse.toggle();
     };
+
+
+
+    //Creating a component
+    $scope.showCreateCompDiv = false;
+    $scope.newCompName = '';
+    $scope.newCompDesc = '';
+    $scope.submitNewComponent = function(){
+      if($scope.newCompName !== ''){
+        console.log('submit new component');
+        //TODO leaderId is not used, as there is no user management yet
+        var component = {description: $scope.newCompDesc, name: $scope.newCompName, leaderId: 1, projectId: $scope.activeProject.id};
+        console.log(component);
+        //TODO ask adam why the component is not created
+        reqBazService.createComponent($scope.activeProject.id,component)
+          .success(function (message) {
+            //console.log('success');
+            console.log(message);
+          })
+          .error(function (error) {
+            console.log(error.message);
+          });
+      }else{
+        console.log('Input field empty');
+        //TODO Show toast
+      }
+    };
+    $scope.clearComponentSubmit = function(){
+      $scope.newCompName = '';
+      $scope.newCompDesc = '';
+      $scope.showCreateCompDiv = false;
+    };
+
+
+    //Creating a requirement
+    $scope.showCreateReqDiv = false;
+    $scope.newReqName = '';
+    $scope.newReqDesc = '';
+    $scope.submitNewReq = function(){
+      if($scope.newReqName !== '' && $scope.newReqDesc !== ''){
+        console.log('submit requirement');
+        //var requirement = {};
+        //reqBazService.createRequirement($scope.activeProject.id,$scope.activeComponent.id,{})
+        //  .success(function () {
+        //    console.log('success');
+        //  })
+        //  .error(function (error) {
+        //    console.log(error.message);
+        //  });
+
+        $scope.showCreateReqDiv = false;
+        //this.createRequirement = function(projectId, componentId, requirement){
+        //  var reqUrl = url + 'projects/' + projectId + '/components/' + componentId + '/requirements';
+        //  return $http.post(reqUrl, requirement);
+        //};
+
+      }else{
+        console.log('Input field empty');
+        //TODO Show toast
+      }
+    };
+    $scope.clearReqSubmit = function(){
+      $scope.newReqName = '';
+      $scope.newReqDesc = '';
+      $scope.showCreateReqDiv = false;
+    };
+
+
+
 
     $scope.signOut = function(){
       window.alert('TODO sign out');
