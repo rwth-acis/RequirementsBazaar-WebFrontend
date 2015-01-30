5954ed9334ab92ad50db02d6d3f3bb425c3b65e9
Kristjan <liiva.kristjan+github@gmail.com>
2015-01-30 12:32:11 +0100

    Cross browser UI fixes.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,57 +10,6 @@
 angular.module('requirementsBazaarWebFrontendApp')
     .controller('MainCtrl', function ($scope, reqBazService, CommentService, ComponentService, $rootScope, $upload) {
 
-    /*
-    * Currently only images, videos and pdfs are accepted as attachments
-    * Called: when the user has selected a file
-    * */
-    $scope.attachments = [];
-    $scope.fileSelected = function (files) {
-      if(files[0].type.indexOf('image/') > -1){
-        $scope.attachments.push({'file':files[0],'URL':URL.createObjectURL(files[0])});
-      }
-      if(files[0].type.indexOf('application/pdf') > -1 || files[0].type.indexOf('video/') > -1){
-        console.log('is pdf or video');
-        $scope.attachments.push({'file':files[0],'URL':null});
-      }
-      console.log($scope.attachments);
-    };
-
-
-    //$scope.upload = $upload.upload({
-    //  url: 'server/upload/url',
-    //  data: {myObj: $scope.myModelObj},
-    //  file: $scope.files
-    //}).progress(function(evt) {
-    //  console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-    //}).success(function(data, status, headers, config) {
-    //  console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-    //});
-
-    /*
-    * After the user has finished editing
-    * Called: by the user, by clicking on submit requirement
-    * */
-    $scope.submitAttachment = function(req){
-      console.log('save changes');
-      console.log('text : '+req.description);
-      if($scope.attachments !== null){
-        console.log('with attachments:');
-        console.log($scope.attachments);
-      }
-      //TODO update req text
-      //TODO save all the attachments
-      //reqBazService.createAttachment()
-      //  .success(function(){
-      //
-      //  })
-      //  .error(function(){
-      //    console.log('error')
-      //  });
-    };
-
-
-
     //Index which requirement in the list is selected
     $scope.selectedIndex = -1;
 
@@ -432,6 +381,83 @@ angular.module('requirementsBazaarWebFrontendApp')
     $scope.deleteComment = function(id,req){
       CommentService.deleteComponent(id,req);
     };
+
+
+    /*
+    * Everything related to attachments
+    *
+    * */
+
+
+    /*
+     * Currently only images, videos and pdfs are accepted as attachments
+     * Called: when the user has selected a file
+     * */
+    $scope.attachments = [];
+    $scope.fileSelected = function (files) {
+      if(files[0].type.indexOf('image/') > -1){
+        $scope.attachments.push({'file':files[0],'URL':URL.createObjectURL(files[0])});
+      }
+      if(files[0].type.indexOf('application/pdf') > -1 || files[0].type.indexOf('video/') > -1){
+        console.log('is pdf or video');
+        $scope.attachments.push({'file':files[0],'URL':null});
+      }
+      console.log($scope.attachments);
+    };
+
+
+    //$scope.upload = $upload.upload({
+    //  url: 'server/upload/url',
+    //  data: {myObj: $scope.myModelObj},
+    //  file: $scope.files
+    //}).progress(function(evt) {
+    //  console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
+    //}).success(function(data, status, headers, config) {
+    //  console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
+    //});
+
+    /*
+     * After the user has finished editing
+     * Called: by the user, by clicking on submit requirement
+     * */
+    $scope.submitAttachment = function(req){
+      console.log('save changes');
+      console.log('text : '+req.description);
+      if($scope.attachments !== null){
+        console.log('with attachments:');
+      }
+
+      console.log($scope.attachments);
+      $rootScope.showFeedback('Warning: Not implemented');
+
+      //TODO update req text
+      //TODO save all the attachments
+
+      //var url = 'http://localhost:8080/bazaar/';
+      //var attachmentUrl = url + 'projects/' + 0 + '/components/' + 0 + '/requirements/' + 0 + '/attachments';
+      //attachmentUrl+= '?attachmentType=F';
+      //
+      //$scope.upload = $upload.upload({
+      //  url: attachmentUrl,
+      //  data: {myObj: $scope.myModelObj},
+      //  file: $scope.files
+      //}).progress(function(evt) {
+      //  console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
+      //}).success(function(data, status, headers, config) {
+      //  console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
+      //});
+
+
+      //this.createAttachment = function(attachmentType, attachment){
+      //  var attachmentUrl = url + 'projects/' + 0 + '/components/' + 0 + '/requirements/' + 0 + '/attachments';
+      //
+      //  attachmentUrl+= '?attachmentType' + attachmentType;
+      //
+      //  return $http.post(attachmentUrl, attachment);
+      //};
+
+    };
+
 
 
     /*
