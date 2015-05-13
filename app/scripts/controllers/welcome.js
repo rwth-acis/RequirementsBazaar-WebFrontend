45cf74c8a0c1b5390f07cfd9cc7b26de3a1cf833
Kristjan <liiva.kristjan+github@gmail.com>
2015-05-13 11:49:31 +0200

    Welcome page new design with sidebar and graph placeholders.

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -8,7 +8,7 @@
  * Controller of the requirementsBazaarWebFrontendApp
  */
 angular.module('requirementsBazaarWebFrontendApp')
-  .controller('WelcomeCtrl', function ($scope, oauthConfig, $location){
+  .controller('WelcomeCtrl', function ($scope, oauthConfig, $location, reqBazService, UtilityService){
     $scope.oauthSite = oauthConfig.SITE;
     $scope.oauthClientId = oauthConfig.CLIENT_ID;
     $scope.oauthDataScope = oauthConfig.DATA_SCOPE;
@@ -16,8 +16,24 @@ angular.module('requirementsBazaarWebFrontendApp')
     $scope.oauthProfileURI = oauthConfig.PROFILE_URI;
     $scope.oauthScope = oauthConfig.SCOPE;
 
-    $scope.goToMain = function(){
-      $location.path('/project/1', true);
-    }
+    /*
+     * Loads projects
+     * Called: only when the page loads
+     * */
+    (function(){
+      reqBazService.getProjects()
+        .success(function (projs) {
+          $scope.projects = projs;
+          if(projs.length === 0){
+            UtilityService.showFeedback('NO_PROJ_EXISTS');
+          }
+        })
+        .error(function () {
+          UtilityService.showFeedback('WARN_PROJS_NOT_LOADED');
+        });
+    })();
 
+    $scope.selectProj = function(project){
+      $location.path('/project/'+project.id, true);
+    };
   });
