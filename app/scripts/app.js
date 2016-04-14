caa62895e17f4544b1b83a0b6ed2e05c58f698ae
albisema <albi.sema@rwth-aachen.de>
2016-04-14 17:47:09 +0200

    fixed drawer style in mobile view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@
     if (window.location.port === '') {  // if production
         // Uncomment app.baseURL below and
         // set app.baseURL to '/your-pathname/' if running from folder in production
-        //app.baseUrl = '/beta/';
+        app.baseUrl = '/beta/';
     }
     /**
      * Whether the user is logged in to the OIDC server.
@@ -31,6 +31,7 @@
     app.hresponse = null;
     app.currentUser = null;
     app.compResponse = null;
+    app.isMobile = false; //initiate as false
 
     app.displayInstalledToast = function() {
         // Check to make sure caching is actually enabled—it won't be in the dev environment.
@@ -42,7 +43,12 @@
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
     app.addEventListener('dom-change', function() {
-        console.log('Our app is ready to rock!');
+        // device detection
+        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+
+            this.isMobile = true;
+        }
     });
 
     // See https://github.com/Polymer/polymer/issues/1381
@@ -96,7 +102,7 @@
      * @returns {string} the URL of the resource to query.
      */
     app.getProjectURL = function(projectId) {
-        return 'https://requirements-bazaar.org/bazaar/projects/' + projectId;
+        return 'https://requirements-bazaar.org/betabazaar/projects/' + projectId;
     };
 
     /**
@@ -106,7 +112,7 @@
      * @returns {string} the URL of the resource to query.
      */
     app.getComponentURL = function(componentId) {
-        return 'https://requirements-bazaar.org/bazaar/components/' + componentId;
+        return 'https://requirements-bazaar.org/betabazaar/components/' + componentId;
     };
 
     /**
@@ -272,15 +278,20 @@
 
     app.toggCompDrawer = function(e){
         //closes left drawer
-        if (document.querySelectorAll('#drawer')[3].style.display != 'none'){
-            app.$.reqDrawer.closeDrawer();
-            document.querySelectorAll('#drawer')[3].style.display = 'none';
-            document.querySelectorAll('#main')[3].style.left = '0px';
+        if (!this.isMobile) {
+            if (document.querySelectorAll('#drawer')[3].style.display != 'none'){
+                //app.$.reqDrawer.closeDrawer();
+                document.querySelectorAll('#drawer')[3].style.display = 'none';
+                document.querySelectorAll('#main')[3].style.left = '0px';
+            } else {
+                //opens left drawer
+                document.querySelectorAll('#drawer')[3].style.display = 'block';
+                //app.$.reqDrawer.openDrawer();
+                document.querySelectorAll('#main')[3].style.left = '256px';
+            }
         } else {
-            //opens left drawer
-            document.querySelectorAll('#drawer')[3].style.display = 'block';
-            app.$.reqDrawer.openDrawer();
-            document.querySelectorAll('#main')[3].style.left = '256px';
+            this.$.reqDrawer.forceNarrow = true;
+            this.$.reqDrawer.togglePanel();
         }
     };
 
@@ -289,34 +300,43 @@
         //opens right drawer
         if (document.querySelector('#drawer').style.display != 'block'){
             document.querySelector('#drawer').style.display = 'block';
-            document.querySelector('#scrollProjects').style.marginRight = '256px';
+            document.querySelector('#drawer').style.zIndex = 1;
+            if (!this.isMobile)
+                document.querySelector('#scrollProjects').style.marginRight = '256px';
             app.$.notDrawer0.openDrawer();
         } else {
             //closes right drawer
             document.querySelector('#drawer').style.display = 'none';
-            document.querySelector('#scrollProjects').style.marginRight = '0px';
+            if (!this.isMobile)
+                document.querySelector('#scrollProjects').style.marginRight = '0px';
             app.$.notDrawer0.closeDrawer();
         }
 
         if (document.querySelectorAll('#drawer')[1].style.display != 'block'){
             document.querySelectorAll('#drawer')[1].style.display = 'block';
-            document.querySelector('#scrollComponents').style.marginRight = '256px';
+            document.querySelectorAll('#drawer')[1].style.zIndex = 1;
+            if (!this.isMobile)
+                document.querySelector('#scrollComponents').style.marginRight = '256px';
             app.$.notDrawer1.openDrawer();
         } else {
             //closes right drawer
             document.querySelectorAll('#drawer')[1].style.display = 'none';
-            document.querySelector('#scrollComponents').style.marginRight = '0px';
+            if (!this.isMobile)
+                document.querySelector('#scrollComponents').style.marginRight = '0px';
             app.$.notDrawer1.closeDrawer();
         }
 
         if (document.querySelectorAll('#drawer')[2].style.display != 'block'){
             document.querySelectorAll('#drawer')[2].style.display = 'block';
-            document.querySelectorAll('#main')[3].style.right = '256px';
+            document.querySelectorAll('#drawer')[2].style.zIndex = 1;
+            if (!this.isMobile)
+                document.querySelectorAll('#main')[3].style.right = '256px';
             app.$.notDrawer.openDrawer();
         } else {
             //closes right drawer
             document.querySelectorAll('#drawer')[2].style.display = 'none';
-            document.querySelectorAll('#main')[3].style.right = '0px';
+            if (!this.isMobile)
+                document.querySelectorAll('#main')[3].style.right = '0px';
             app.$.notDrawer.closeDrawer();
         }
     };
@@ -338,7 +358,7 @@
         var title = document.querySelector('.editForm > .editTitle').value;
         var desc = document.querySelector('.editForm > .editDesc').value;
         var el = document.getElementById('postUpdateProject');
-        el.url = "https://requirements-bazaar.org/bazaar/projects/" + this.params.projectId;
+        el.url = "https://requirements-bazaar.org/betabazaar/projects/" + this.params.projectId;
         if (title == '' || desc == '' || title == null || desc == null ){
             this.$.superToast.text = "You can't have empty fields!";
             this.$.superToast.open();
@@ -386,7 +406,7 @@
         if (e.detail.confirmed) {
             var request = document.getElementById('postUpdateProject');
 
-            request.url = "https://requirements-bazaar.org/bazaar/projects/" + this.params.projectId;
+            request.url = "https://requirements-bazaar.org/betabazaar/projects/" + this.params.projectId;
             request.method = "DELETE";
             request.generateRequest();
         }
@@ -394,7 +414,7 @@
 
     app.setDefault = function(e){
         var req = this.$.postUpdateProject;
-        req.url = "https://requirements-bazaar.org/bazaar/projects/" + this.params.projectId;
+        req.url = "https://requirements-bazaar.org/betabazaar/projects/" + this.params.projectId;
         req.method = "PUT";
         req.body = JSON.stringify({
             "id": parseInt(this.params.projectId),
@@ -421,7 +441,7 @@
         var title = document.querySelector('.editFormComp > .editTitle').value;
         var desc = document.querySelector('.editFormComp > .editDesc').value;
         var el = document.getElementById('componentRequestPUT');
-        el.url = "https://requirements-bazaar.org/bazaar/components/" + this.params.componentId;
+        el.url = "https://requirements-bazaar.org/betabazaar/components/" + this.params.componentId;
         if (title == '' || desc == '' || title == null || desc == null ){
             this.$.superToast.text = "You can't have empty fields!";
             this.$.superToast.open();
@@ -454,7 +474,7 @@
         if (e.detail.confirmed) {
             var request = document.getElementById('componentRequest');
 
-            request.url = "https://requirements-bazaar.org/bazaar/components/" + this.params.componentId;
+            request.url = "https://requirements-bazaar.org/betabazaar/components/" + this.params.componentId;
             request.method = "DELETE";
             request.generateRequest();
         }
