5dc474be7ba50ae275e40df252ca2030754a92b5
albisema <albi.sema@rwth-aachen.de>
2016-11-22 18:45:58 +0100

    commenting methods 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,15 +39,28 @@
      * @type {string}
      */
     app.access_token = null;
-    app.hresponse = null;
     /**
      * Stores the JSON of the API call to <tt>/users/current</tt> that is retrieved via an iron-ajax.
      *
      * @type {string} a JSON object containing the currently logged in user, or null, if not logged in.
      */
     app.currentUser = null;
+    /**
+     * True if page is opened on mobile, else false. It is used to style page properly or to hide certain elements
+     * on mobile view
+     * @type {boolean}
+     */
     app.isMobile = false; //initiate as false
+    /**
+     * Save the i18-msg dom element and it is used to get messages in different languages
+     * @type {null}
+     */
     app.i18n = null;
+    /**
+     * It is true only when iron-ajax is loading for getting projects components and requirements
+     * Used to show progress bar when xhr on flight
+     * @type {boolean}
+     */
     app.loading = false;
     /**
      * Defines whether the requirements list shows active or realized requirements.
@@ -70,9 +83,13 @@
      * @type {string} 'list' for list, 'grid' for grid.
      */
     app.view = "list";
+    /**
+     * If a user follows a component or a Project it becomes true. DEPRECATED
+     * TO-DO: REPLACE WITH A BETTER METHOD
+     * @type {boolean}
+     */
     app.isFollowerProj = false;
     app.followsComp = false;
-    app.code = null;
 
 
     app.displayInstalledToast = function() {
@@ -85,7 +102,7 @@
     // Listen for template bound event to know when bindings
     // have resolved and content has been stamped to the page
     app.addEventListener('dom-change', function() {
-        // device detection
+        // detect mobile device
         if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
             || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
 
@@ -97,28 +114,29 @@
     window.addEventListener('WebComponentsReady', function() {
         // imports are loaded and elements have been registered
         this.i18n = document.querySelector('i18n-msg');
+        //property used to check when the components are ready so it can scroll to requirement
         app.loaded = true;
     });
 
-    window.addEventListener('storage', function(e) {
-        if (e.key === "code"){
-            app.code = e.newValue;
-            app.$.settingsDialog.close();
-            // app.getGithubAccessToken();
-        }
-    });
-
+    /**
+     * It is called when all the html imports are done. It checks for properties that are saved in the cookie,
+     * such as language and view type: list or grid. If cookie doesn't have information saved on loads browser language
+     * and list view as default
+     */
     document.addEventListener('HTMLImportsLoaded', function() {
         var lang = null;
         var view = null;
-        if (document.cookie != ''){
+        if (document.cookie != '') {
+            //array with cookies values
             var cookies = document.cookie.split(';');
-            for(var i = 0; i <cookies.length; i++) {
+            for (var i = 0; i <cookies.length; i++) {
+                //current parameter
                 var c = cookies[i];
+                //remove before space if it has
                 while (c.charAt(0)==' ') {
                     c = c.substring(1);
                 }
-                console.log(c);
+                //get the parameters from the cookie c
                 if (c.indexOf("lang=") == 0) {
                     lang = c.substring("lang=".length, c.length);
                 }
@@ -131,8 +149,9 @@
                 }
             }
         }
-        
-        if (lang === null || lang ===''){
+
+        //load browser language as default language
+        if (lang === null || lang ==='') {
             switch (navigator.language.substring(0,2)) {
                 case "en":
                     I18nMsg.lang = 'en';
