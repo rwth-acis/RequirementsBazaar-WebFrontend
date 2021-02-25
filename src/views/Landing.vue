f0367c4f88ccd393f7c5eddc05cb51b9614fbb82
istvank <istvank@gmx.de>
2021-02-25 16:19:21 +0100

    Adding statement about beta

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>Welcome to Requirements Bazaar!</h1>
+  <h2 style="color:red;">
+    This is the BETA environment of Requirements Bazaar, not intended for productive usage, we might delete data here at any time.
+    For productive use (even if it's just a test project) please go to the main instance.
+  </h2>
   <router-link to="/projects">Projects List</router-link>
 </template>
 
