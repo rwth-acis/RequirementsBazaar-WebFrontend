8b42bf045875ac3cc9672486ffcdb06693533c0d
istvank <istvank@gmx.de>
2021-04-04 21:45:57 +0200

    ActivityTracker optional

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="layout-wrapper layout-static layout-static-sidebar-inactive">
     <AppTopbar></AppTopbar>
 
-    <div id="layout">
+    <div id="layout" :class="{ 'p-mr-3': !activityTrackerVisible }">
       <div id="container">
         <div id="content">
           <router-view></router-view>
@@ -11,14 +11,14 @@
           <router-link to="/about">About</router-link> <router-link to="/developer">Developer</router-link> <router-link to="/privacy">Privacy</router-link>
         </footer>
       </div>
-      <div id="activityTrackerPlaceholder"></div>
-      <ActivityTracker class="activityTracker"></ActivityTracker>
+      <div id="activityTrackerPlaceholder" v-if="activityTrackerVisible"></div>
+      <ActivityTracker class="activityTracker" v-if="activityTrackerVisible"></ActivityTracker>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import AppTopbar from './AppTopbar.vue';
 import ActivityTracker from './components/ActivityTracker.vue';
 
@@ -27,6 +27,10 @@ export default defineComponent({
   components: {
     AppTopbar,
     ActivityTracker
+  },
+  setup: () => {
+    const activityTrackerVisible = ref(false);
+    return { activityTrackerVisible };
   }
 })
 </script>
