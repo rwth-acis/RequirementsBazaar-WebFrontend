408f74cc3afd3ec061e688045773f4ea8f1d5a1d
istvank <istvank@gmx.de>
2021-04-18 17:44:49 +0200

    Add i18n to landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,14 +1,14 @@
 <template>
   <h1>{{ t('landing-welcome') }}</h1>
   <h2 style="color:red;">
-    This is the BETA environment of Requirements Bazaar, not intended for productive usage, we might delete data here at any time.
+    This is the BETA environment of Requirements Bazaar, not intended for production usage, we might delete data here at any time.
     For production usage (even if it's just a test project) please go to the main instance.
   </h2>
-  <router-link to="/projects">Projects List</router-link>
+  <router-link to="/projects">{{ t('explore-projects') }}</router-link>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
