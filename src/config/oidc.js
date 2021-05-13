f46791f0859a06afc46acf3948032c9521153b53
istvank <istvank@gmx.de>
2021-05-13 15:10:38 +0200

    Fix popupRedirectUri

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -2,7 +2,7 @@ export const oidcSettings = {
   authority: 'https://api.learning-layers.eu/o/oauth2',
   clientId: 'c7588efc-f831-4e31-928e-0f46a91fb311',
   redirectUri: `${import.meta.env.VITE_OIDC_CALLBACK_URL}/oidc-callback`,
-  popupRedirectUri: `${import.meta.env.VITE_OIDC_CALLBACK_URL}//oidc-popup-callback`,
+  popupRedirectUri: `${import.meta.env.VITE_OIDC_CALLBACK_URL}/oidc-popup-callback`,
   responseType: 'id_token token',
   scope: 'openid email profile',
   automaticSilentRenew: true,
