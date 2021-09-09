7cc865fc0c7f4f2119ced8b775d4c8b055555e23
istvank <istvank@gmx.de>
2021-09-09 11:57:10 +0200

    Initial landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,12 +1,39 @@
 <template>
-  <h1>{{ t('landing-welcome') }}</h1>
-  <h2 style="color:red;">
+  <h1 style="text-align: center">{{ t('landing-welcome') }}</h1>
+  <!--<h2 style="color:red;">
     This is the BETA environment of Requirements Bazaar, not intended for production usage (there might be bugs!), we might delete data here at any time.
     For production usage (even if it's just a test project) please go to the main instance on <a href="https://requirements-bazaar.org">https://requirements-bazaar.org</a> (without the "beta" in the URL).
     <br />
     <br />
     NEXT DATA RESET (current grace period): September 30, 2021
-  </h2>
+  </h2>-->
+  <div class="p-grid">
+    <div class="p-col">
+      <img src="/reqbaz-responsive.png" style="max-width: 400px;">
+    </div>
+    <div class="p-col">
+      <h2>{{ t('lp-title') }}</h2>
+      <div class="description">
+        {{ t('lp-desc') }}
+      </div>
+      <router-link to="/projects">
+        <Button :label="t('explore-projects')" />
+      </router-link>
+    </div>
+  </div>
+
+  <h2>{{ t('landing-how-it-works') }}</h2>
+  <div class="description">
+    <div>{{ t('landing-step-1') }}</div>
+    <div>{{ t('landing-step-2') }}</div>
+    <div>{{ t('landing-step-3') }}</div>
+  </div>
+
+  <h2>{{ t('landing-try-it-out') }}</h2>
+  <div class="description">
+    <div>{{ t('takeAlook') }}</div>
+  </div>
+
   <router-link to="/projects">
     <Button :label="t('explore-projects')" />
   </router-link>
@@ -42,4 +69,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
+  .description {
+    font-size: 15pt;
+    margin-bottom: 20px;
+  }
 </style>
