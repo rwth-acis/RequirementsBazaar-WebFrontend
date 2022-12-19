5a6d81f40092b25b3407dec147a96f07c71e8404
Tobi <kiel.tobi99@gmail.com>
2022-12-19 10:15:53 +0100

    update tsconfig

--- src/components/ExportPopup.vue
+++ src/components/ExportPopup.vue
@@ -14,12 +14,13 @@
 </template>
 
 <script lang="ts">
-import * as pdfFonts from "pdfmake/build/vfs_fonts.js"; // <-- vfs_fonts has to be imported before pdfmake
-import * as pdfMake from "pdfmake/build/pdfmake";
 import { Requirement } from '@/types/bazaar-api';
 import { defineComponent, PropType, ref } from 'vue'
 import { useI18n } from 'vue-i18n';
 import Dropdown from 'primevue/dropdown';
+import * as pdfFonts from "pdfmake/build/vfs_fonts.js"; // <-- vfs_fonts has to be imported before pdfmake
+import pdfMake from 'pdfmake';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 
 export default defineComponent({
@@ -232,24 +233,7 @@ export default defineComponent({
         }
       };
       // Workaround for fonts as the import suggested by the docs causes error
-      pdfMake.createPdf(docDefinition, {},
-      {
-        // Default font should still be available
-        Roboto: {
-          normal: 'Roboto-Regular.ttf',
-          bold: 'Roboto-Medium.ttf',
-          italics: 'Roboto-Italic.ttf',
-          bolditalics: 'Roboto-Italic.ttf'
-        },
-        // Make sure you define all 4 components - normal, bold, italics, bolditalics - (even if they all point to the same font file)
-        TimesNewRoman: {
-          normal: 'Times-New-Roman-Regular.ttf',
-          bold: 'Times-New-Roman-Bold.ttf',
-          italics: 'Times-New-Roman-Italics.ttf',
-          bolditalics: 'Times-New-Roman-Italics.ttf'
-        }
-      },
-      pdfFonts.pdfMake.vfs).download(fileName);
+      pdfMake.createPdf(docDefinition).download(fileName);
       console.log('Export PDF finished');
     };
 
